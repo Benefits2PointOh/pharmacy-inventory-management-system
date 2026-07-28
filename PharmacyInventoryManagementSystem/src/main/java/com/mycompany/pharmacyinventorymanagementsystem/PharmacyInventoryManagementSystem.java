e5e45b3be13572d361ee8bc9a2919c3e234a083d
Benefits2PointOh <tafaramawereb@gmail.com>
2026-07-28 16:29:43 +0200

    Initialize Maven project

---
+++ PharmacyInventoryManagementSystem/src/main/java/com/mycompany/pharmacyinventorymanagementsystem/PharmacyInventoryManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pharmacyinventorymanagementsystem;
+
+/**
+ *
+ * @author Benefits
+ */
+public class PharmacyInventoryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
